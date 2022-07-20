393586cceb3f22dd19340da6eb19f140cda1c794
Simon <simon@banyard.org>
2022-07-20 08:32:04 +0100

    added loop for 'members' export

--- ProfileGroupBackup.py
+++ ProfileGroupBackup.py
@@ -4,17 +4,21 @@ import pandas as pd
 
 import mimecast as mc
 
+# Add endpoints
 find_groups = "/api/directory/find-groups"
 members = "/api/directory/get-group-members"
 
-data = str({"meta": {"pagination": {"pageSize": 500}}})
+# Request body
+find_groups_data = str({"meta": {"pagination": {"pageSize": 500}}})
 
-groups = mc.send_request(find_groups, data)
+# Make request
+groups = mc.send_request(find_groups, find_groups_data)
 
-# Export to Excel with Pandas
+# Check that a directory exists, if it doesnt, create it
 if not os.path.isdir("data"):
     os.mkdir("data")
 
+# Export data to Excel with Pandas
 groups_df = pd.DataFrame(groups["data"][0]["folders"])
 groups_df.to_excel(
     "data/profile_groups.xlsx",
@@ -22,8 +26,10 @@ groups_df.to_excel(
     columns=["description", "userCount", "folderCount", "id"],
 )
 
+# Create a dictionary of ID's for group backup
 group_ids = dict(zip(groups_df["description"], groups_df["id"]))
 
+# Loop over dictionary
 for group_id in group_ids:
     data = str(
         {
@@ -32,9 +38,33 @@ for group_id in group_ids:
         }
     )
     profile_groups = mc.send_request(members, data)
-
     print(group_id, "\n")
     profile_groups_df = pd.DataFrame(profile_groups["data"][0]["groupMembers"])
+
+    if profile_groups.get("meta").get("pagination").get("next"):
+        page_token = profile_groups.get("meta").get("pagination").get("next")
+
+    while "next" in profile_groups.get("meta").get("pagination"):
+        data = str(
+            {
+                "meta": {
+                    "pagination": {
+                        "pageSize": 500,
+                        "pageToken": page_token
+                    }
+                },
+                    "data": [
+                    {
+                        "id": group_ids[group_id]
+                    }
+                ]
+            }
+        )
+        profile_groups = mc.send_request(members, data)
+        profile_groups_df = pd.concat([profile_groups_df, pd.DataFrame(profile_groups["data"][0]["groupMembers"])])
+        if profile_groups.get("meta").get("pagination").get("next"):
+            page_token = profile_groups.get("meta").get("pagination").get("next")
+
     profile_groups_df.to_excel(
         f"data/{group_id}.xlsx",
         index=False,
